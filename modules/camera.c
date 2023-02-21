509689674e5991ced6acc33223f05801aa1d8f81
Scott Thibault <sathibault@yahoo.com>
2023-02-21 12:16:55 -0500

    Move configuration data; Move camera/display setup to power_on functions.

--- modules/camera.c
+++ modules/camera.c
@@ -22,9 +22,59 @@
  * PERFORMANCE OF THIS SOFTWARE.
  */
 
-#include "monocle.h"
 #include "nrf_gpio.h"
+#include "nrfx_systick.h"
 #include "py/runtime.h"
+
+#include "monocle.h"
+#include "camera-config.h"
+#include "fpga-lib.h"
+
+
+STATIC mp_obj_t camera_power_on() {
+  // Start the camera clock
+  if (!fpga_has_feature(camera_feat))
+    mp_raise_ValueError(MP_ERROR_TEXT("Camera not available"));
+
+  uint8_t cam_dev = fpga_feature_dev(camera_feat);
+  uint8_t command[2] = {cam_dev, 0x09};
+  spi_write(FPGA, command, 2, false);
+
+  // Reset sequence taken from Datasheet figure 2-3
+  nrf_gpio_pin_write(CAMERA_RESET_PIN, false);
+  nrf_gpio_pin_write(CAMERA_SLEEP_PIN, true);
+  nrfx_systick_delay_ms(5); // t2
+  nrf_gpio_pin_write(CAMERA_SLEEP_PIN, false);
+  nrfx_systick_delay_ms(1); // t3
+  nrf_gpio_pin_write(CAMERA_RESET_PIN, true);
+  nrfx_systick_delay_ms(20); // t4
+
+  // Read the camera CID (one of them)
+  i2c_response_t resp = i2c_read(CAMERA_I2C_ADDRESS, 0x300A, 0xFF);
+  if (resp.fail || resp.value != 0x56)
+    {
+      NRFX_LOG_ERROR("Error: Camera not found.");
+      return mp_obj_new_bool(false);
+    }
+
+  // Software reset
+  i2c_write(CAMERA_I2C_ADDRESS, 0x3008, 0xFF, 0x82);
+  nrfx_systick_delay_ms(5);
+
+  // Send the default configuration
+  for (size_t i = 0;
+       i < sizeof(camera_config) / sizeof(camera_config_t);
+       i++)
+    {
+      i2c_write(CAMERA_I2C_ADDRESS,
+		camera_config[i].address,
+		0xFF,
+		camera_config[i].value);
+    }
+
+  return mp_obj_new_bool(true);
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_0(camera_power_on_obj, camera_power_on);
 
 STATIC mp_obj_t camera_sleep(void)
 {
@@ -42,6 +92,7 @@ STATIC MP_DEFINE_CONST_FUN_OBJ_0(camera_wake_obj, camera_wake);
 
 STATIC const mp_rom_map_elem_t camera_module_globals_table[] = {
 
+    {MP_ROM_QSTR(MP_QSTR_power_on), MP_ROM_PTR(&camera_power_on_obj)},
     {MP_ROM_QSTR(MP_QSTR_sleep), MP_ROM_PTR(&camera_sleep_obj)},
     {MP_ROM_QSTR(MP_QSTR_wake), MP_ROM_PTR(&camera_wake_obj)},
 };
